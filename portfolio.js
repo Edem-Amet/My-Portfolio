fc247773dbac026b18fa4a17a354204e8ae66ed0
Edem-Amet <143708044+Edem-Amet@users.noreply.github.com>
2023-08-31 17:26:22 +0000

    Add files via upload

---
+++ portfolio.js
@@ -0,0 +1,7 @@
+var typed = new Typed(".text", {
+    strings: ["Frontend Developer", "Mobile App Developer", "Space-guy"],
+    typeSpeed: 30,
+    backSpeed: 30,
+    backDelay: 1000,
+    loop: true
+});
